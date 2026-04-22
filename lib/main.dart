bc0974316f9ff8b4f62ec394b16af5f020e68913
Enrique Díaz Valenzuela <diazvalenzuelaenrique@gmail.com>
2026-04-22 22:09:15 +0200

    aprendiendo flutter 2

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,51 @@ class MyApp extends StatelessWidget {
   //Sin esto mi widget no sabria que pintar
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: MyHomePage(titulo: 'Scout App', version: '1.0.0'),
+    );
+  }
+}
+
+// Scaffold
+// Es solo la cáscara del widget. Aquí van los parámetros que le pasas desde fuera
+class MyHomePage extends StatefulWidget {
+  final String titulo;
+  final String version;
+
+    const MyHomePage({super.key, required this.titulo, required this.version});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+// Contenido
+// Aquí vive todo lo demás: los datos internos que cambian (_counter), la lógica, y el build(). 
+//El guión bajo del principio _ significa que es privada — solo accesible dentro de este archivo.
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('${widget.titulo} v${widget.version}'),
+              Text('$_counter'),
+            ],
+          ),
+        ),
+      
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _counter++;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
